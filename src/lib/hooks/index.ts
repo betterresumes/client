934bf0abcb1d56e5815a0491f8641f328c568c86
pranitpatil03 <patilpranit3112@gmail.com>
2025-09-20 23:00:57 +0530

    refacotr

--- src/lib/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-// Query hooks exports
-export * from './use-companies'
-export * from './use-predictions'
-export * from './use-jobs'
-
-// Re-export query client utilities
-export { queryClient, queryKeys, invalidateQueries } from '../config/query-client'
--- src/lib/stores/index.ts
+++
@@ -1,6 +0,0 @@
-// Store exports
-export { useAuthStore } from './auth'
-export { useUIStore, useNotifications, useLoading } from './ui'
-
-// Re-export types for convenience
-export type { User, AuthTokens } from '../types/auth'
